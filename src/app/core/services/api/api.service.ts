d6f8b98108bc028ee67806adf83e698a9c24e2b1
didit02 <adityarizkyputra@gmail.com>
2020-02-15 18:22:45 +0700

    updated

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable , throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, catchError} from "rxjs/operators";
+import { map} from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
-
+export class ApiService {
   headers = new HttpHeaders({ 
-    'content-type': 'application/json'
+    'content-type': 'application/json', 
+    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('currentUser')).token
   });
   options = { headers: this.headers };
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    ) { }
+
   handleError(error) {
     let errorMessage = '';
     if (error.error instanceof ErrorEvent) {
@@ -22,9 +24,9 @@ export class AuthenticationService {
     return throwError(errorMessage);
   }
 
-  login(username, password): Observable<any> {
+  postdatakoperasi(a,b): Observable<any> {
     const url = 'http://dev-api.peers.id/public/login';
-    let body = { "email": username, "password": password }
+    let body = { "a": a, "b": b }
     return this.http.post(url, JSON.stringify(body), this.options).pipe(map(res => res))
   }
 }
