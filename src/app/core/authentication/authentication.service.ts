75588efaccc0dac187bf004a3142ca0e0c920dd4
didit02 <adityarizkyputra@gmail.com>
2020-02-15 18:23:10 +0700

    upload

--- src/app/core/authentication/authentication.service.ts
+++ src/app/core/authentication/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable , throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, catchError} from "rxjs/operators";
+import { map} from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
